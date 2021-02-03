3b5309a84746eb5c62699d458ea0855867ac3132
Matheus Brito <matheus.rja@hotmail.com>
2021-02-02 22:14:13 -0300

    projeto calculadora em python

---
+++ CalculadoraRecode/app.py
@@ -0,0 +1,36 @@
+from flask import Flask,render_template,request
+
+app = Flask(__name__,template_folder="./src/views")
+
+@app.route("/",methods=["GET","POST"])
+def home():
+    if (request.method =="GET"):
+        return render_template("index.html")
+    else:
+        if(request.form["input1"] != "" and request.form["input2"] != ""):
+            num1=request.form["input1"]
+            num2=request.form["input2"]
+            condicionais=request.form["condicionais"]
+      
+      
+            soma = int(num1) + int(num2)
+            subt = int(num1) - int(num2)
+            mult = int(num1) * int(num2)
+            divi=int(num1) / int(num2)
+
+            if(condicionais == "soma"):    
+               return str(soma)
+            elif(condicionais == "subt"):
+               return str(subt)
+            elif(condicionais == "mult"):
+               return str(mult)
+            else:
+                return str(divi)
+        else:
+            return "<h1>Complete todo o formulario!!!<h1>"
+
+@app.errorhandler(404)
+def note_foud(erro):
+    return render_template("erro.html")
+
+app.run(port=8080, debug=True)
